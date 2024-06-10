306eef8eda5e1ed334cff6030c603bf10ffd3bb3
rupokghosh <rupokghosh291@gmail.com>
2024-06-10 12:22:44 -0700

    update everything with github

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-
+import Input from "../../../components/ui/input";
+import { Button } from "../../../components/ui/button";
 const pages = () => {
-  return <div>pages</div>;
+  return (
+    <div>
+      <form action="">
+        <h1>Learn Academy</h1>
+        <h2>Log in to Learn Academy</h2>
+        <div>
+          <Input />
+          <Input type="password" placeholder="password" />
+        </div>
+        <Button variant="outline">Login</Button>
+        <div>Don't have an account? sign up</div>
+      </form>
+    </div>
+  );
 };
 
 export default pages;
